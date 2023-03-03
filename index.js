fccfa8bb146721833df7cb1087d1ec014da845a7
AhmedMurshid <ahmed.abdullahione@gmail.com>
2023-03-02 21:59:39 -0500

    highlight checker next move yellow

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 const color1 = 0xff0000;
 const color2 = 0x00e5ff;
 const light = 0xfbff00;
-
 let moved;
 
 // function currentPlace(checker) {
@@ -28,11 +27,9 @@ let moved;
 function currentPlace(n) {
   return n;
 }
-
 function isCheckerOfColor(checker, color) {
   return checker.tint === color;
 }
-
 function createChecker(x, y, color) {
   const checker = new PIXI.Graphics();
   checker.beginFill(color);
@@ -48,51 +45,23 @@ function createChecker(x, y, color) {
   checker.buttonMode = true;
   checker.dragging = false;
   let currentDraggedChecker;
-  const validPosX = [0, 1, 2, 3, 4, 5, 6, 7];
-
-  checker.on('mousedown', function (e) {
-    console.log('Picked up', x, y);
-    checker.beginFill(light);
-
-    checker.x = e.data.global.x - tileSize / 2;
-    checker.y = e.data.global.y - tileSize / 2;
-    checker.dragging = true;
-    currentDraggedChecker = checker;
-  });
-  checker.on('mousemove', function (e) {
-    if (currentDraggedChecker === checker) {
-      checker.x = e.data.global.x - tileSize / 2;
-      checker.y = e.data.global.y - tileSize / 2;
-      console.log('Dragging at', checker.x, checker.y);
-    } else {
-      console.log('Not dragging');
-      checker.on('mouseup', function (e) {
-        movements1(checker,x,y);
-        checker.dragging = false;
-        currentDraggedChecker = undefined;
-      });
-    }
-  });
-
-  
-
+  initChecker(checker,x,y);  
   app.stage.addChild(checker);
 }
-function movements1(checker,x,y){
-  // const newPosX = currentPlace(x);
-  console.log("movement function active");
-  if (isCheckerOfColor(checker, color1) || isCheckerOfColor(checker, color2)) {
-    console.log("The checker has the color you're looking for.", color1);
-    console.log(x, "x place");
-    checker.x = currentPlace(x) * tileSize;
-    checker.y = currentPlace(y) * tileSize;
-    console.log('Put down', checker.x, checker.y);
-    console.log(currentPlace(x), "something is wrong");
-  } else {
-    console.log("The checker does not have the color you're looking for.", color1,"",checker.tint);
-  }
-}
-
+// function movements1(checker,x,y){
+//   // const newPosX = currentPlace(x);
+//   console.log("movement function active");
+//   if (isCheckerOfColor(checker, color1) || isCheckerOfColor(checker, color2)) {
+//     console.log("The checker has the color you're looking for.", color1);
+//     console.log(x, "x place");
+//     checker.x = currentPlace(x) * tileSize;
+//     checker.y = currentPlace(y) * tileSize;
+//     console.log('Put down', checker.x, checker.y);
+//     console.log(currentPlace(x), "something is wrong");
+//   } else {
+//     console.log("The checker does not have the color you're looking for.", color1,"",checker.tint);
+//   }
+// }
 function createBoard() {
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
@@ -114,7 +83,6 @@ function createBoard() {
     }
   }
 }
-
 function addCheckersToBoard() {
   for (let i = 0; i < 8; i++) {
     gameState[i] = [];
@@ -128,6 +96,78 @@ function addCheckersToBoard() {
     }
   }
 }
+function initChecker(checker, x, y) {
+  checker.on('pointerdown', function (e) {
+    console.log('Picked up', x, y);
+    checker.beginFill(light);
+  
+    checker.x = e.data.global.x - tileSize / 2;
+    checker.y = e.data.global.y - tileSize / 2;
+    checker.dragging = true;
+    currentDraggedChecker = checker;
+    checker.on('pointermove', function (e) {
+      if (currentDraggedChecker === checker) {
+        checker.x = e.data.global.x - tileSize / 2;
+        checker.y = e.data.global.y - tileSize / 2;
+        console.log('Dragging at', checker.x, checker.y);
+      } else {
+        console.log('Not dragging');
+      }
+    });
+    
+    highlightSurroundingTiles(checker); // Call the highlightSurroundingTiles function
+  });
+
+  checker.on('pointerup', function (e) {
+    if (currentDraggedChecker === checker) {
+      checker.dragging = false;
+      checker.x = currentPlace(x) * tileSize;
+      checker.y = currentPlace(y) * tileSize;
+      currentDraggedChecker = undefined;
+     } 
+  });
+}
+
+
+function highlightSurroundingTiles(checker) {
+  // Get the current position of the checker
+  const x = checker.x / tileSize;
+  const y = checker.y / tileSize;
+  
+  // Define the tiles to highlight based on the checker's color and position
+  let highlightTiles = [];
+  if (isCheckerOfColor(checker, color2)) {
+    highlightTiles = [
+      [x - 1, y - 1],
+      [x + 1, y - 1]
+    ];
+  } else if (isCheckerOfColor(checker, color1)) {
+    highlightTiles = [
+      [x - 1, y + 1],
+      [x + 1, y + 1]
+    ];
+  }
+  
+  // Highlight the tiles
+  highlightTiles.forEach(tile => {
+    const tileX = tile[0] * tileSize;
+    const tileY = tile[1] * tileSize;
+    const tileGraphic = new PIXI.Graphics();
+    tileGraphic.beginFill(0xffff00);
+    tileGraphic.drawRect(0, 0, tileSize, tileSize);
+    tileGraphic.endFill();
+    tileGraphic.x = tileX;
+    tileGraphic.y = tileY;
+    app.stage.addChild(tileGraphic);
+    checker.on('pointerout', () => {
+      app.stage.removeChild(tileGraphic);
+    });
+  });
+  
+}
+
+
+
 createBoard();
 addCheckersToBoard();
   
